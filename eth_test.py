c5f51fe16235425fb879313f541ba2a591843489
wangbo <wyatwl@163.com>
2018-07-31 08:23:17 +0800

    以太网测试
    有问题，添加math.h时arm9中提示INF和NAN没有定义，上网查还不知道怎么解决

---
+++ eth_test.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python3
+import socket
+
+s = socket.socket()
+# print(socket.getaddrinfo())
+host = socket.gethostname()
+port = 1235
+ip = socket.gethostbyname("wangbo")
+print(host)
+print(ip)
+# s.bind((host, port))
+s.connect(('192.168.1.3', port))
+# s.listen(5)
+#
+
+msg = s.recv(1024)
+
+s.close()
+
+print(msg)
+
+# while True:
+#     c, addr = s.accept()
+#     print(addr)
+#     s.send("欢迎")
+#     c.close()
